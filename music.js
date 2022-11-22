1bc5e6570ee20abcf54066cb2289402d2c551c9d
Josh <jsig885@gmail.com>
2022-11-21 21:32:36 -0800

    Update

--- music.js
+++ music.js
@@ -6,9 +6,45 @@ const viewShare = document.querySelector(".share-btn");
 popup = document.querySelector(".share-popup");
 close = popup.querySelector(".close-share");
 
+let track_art = document.querySelector(".image-container");
+let track_name = document.querySelector(".title");
+let track_artist = document.querySelector(".artist");
+
+let seek_slider = document.querySelector(".seek-slider");
+let volume_slider = document.querySelector(".volume-bar")
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
 let isPlaying = false;
 let muted = false;
 let liked = false;
+
+
+let track_index = 0;
+let updateTimer;
+
+let curr_track = document.createElement('audio');
+
+let track_list = [
+  {
+    name: "Thinkin Bout You",
+    artist: "Frank Ocean",
+    image: "https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg",
+    song: "songs/ThinkinBoutYou.mp3"
+  },
+  {
+    name: "Salad Days",
+    artist: "Mac Demarco",
+    image: "https://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/Screen_Shot_2017-02-20_at_12.25.56_PM_u0wced/steve-lacy-dark-red-single-steve-lacys-demo-the-internet.jpg",
+    song: "songs/DarkRed.mp3"
+  },
+  {
+    name: "Dark Red",
+    artist: "Steve Lacy",
+    image: "https://upload.wikimedia.org/wikipedia/en/8/81/MacDeMarcoSaladDays.png",
+    song: "songs/DarkRed.mp3"
+  }
+]
 
 viewShare.onclick = ()=>{
   popup.classList.toggle("show");
@@ -75,6 +111,72 @@ function playpauseTrack() {
     mute_btn.innerHTML = '<i class="fas fa-volume-down"></i>';
   }
 
-  function shareTab() {
-    console.log("Sharing shit lfg!");
+  function resetValues() {
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
   }
+
+function loadTrack(track_index) {
+    clearInterval(updateTimer);
+    resetValues();
+    curr_track.src = track_list[track_index].song;
+    curr_track.load();
+  
+    track_art.innerHTML = '<img href="" alt="cover"' 
+    track_name.textContent = track_list[track_index].name;
+    track_artist.textContent = track_list[track_index].artist;
+  
+    updateTimer = setInterval(seekUpdate, 1000);
+    curr_track.addEventListener("ended", nextTrack);
+}
+
+loadTrack(track_index);
+
+function nextTrack() {
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
