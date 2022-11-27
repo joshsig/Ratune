9aea285b165396c6e1104199966c260a1b092c95
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2022-11-26 23:16:10 -0600

    Added the fillter and searchbar into explore pg

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,3 @@
-function setting(){
-    confirm("We are sorry.\nThis feature is under maintenance.\nIf you have any issues, you are welcome to contact us by sending email to -> sigurd35@myumanitoba.ca <-\nThank you so much.");
-}
-
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -15,9 +11,46 @@ function readURL(input) {
     }
 }
 
+function setting(){
+    confirm("We are sorry.\nThis feature is under maintenance.\nIf you have any issues, you are welcome to contact us by sending email to -> sigurd35@myumanitoba.ca <-\nThank you so much.");
+}
+
+function openFilterMenu(){
+    document.getElementById("fil_Content").style.visibility="visible";
+    document.getElementById("fil_Content").style.opacity="1";
+}
+
+function closeFilterMenu(){
+    document.getElementById("fil_Content").style.opacity="0";
+    document.getElementById("fil_Content").style.visibility="hidden";
+}
+
+function startFil(){
+    confirm("This feature is implementing.")
+}
+
 function logout(){
     localStorage.removeItem("array_music");
     location.assign("./index.html");
+}
+
+const searchInput=document.querySelector("[target]");
+searchInput.addEventListener("input", e => {
+    const value=e.target.value
+    console.log(value)
+});
+
+document.addEventListener('click', function handleClickOutsideBox(event) {
+    const searchFeature = document.getElementById("search_Bar");
+    const searchResult = document.getElementById("search_Result");
+
+    if (!searchFeature.contains(event.target)) {
+        searchResult.style.visibility="hidden";
+    }
+});
+
+function searchDropdown(){
+    document.getElementById("search_Result").style.visibility="visible";
 }
 
 let array_acc=JSON.parse(localStorage.getItem("thisAcc"));
