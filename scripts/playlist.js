d02188fe5b509228aec7ea572c3d5e0d24f0807f
Josh <jsig885@gmail.com>
2022-11-26 18:10:04 -0800

    Added Friends bar

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -2,12 +2,13 @@ const playAdd = document.querySelector('.playlist-add');
 const playmodal = document.querySelector('.playlist-modal');
 const savePlaylist = document.querySelector('.save-playlist');
 const container = document.querySelector('.playlist-container');
-const saveBtn = document.querySelector('.save-playlist');
 
 playAdd.onclick = ()=>{
     playmodal.classList.toggle("show");
     container.classList.toggle("blur");
   }
-  savePlaylist.onclick = ()=>{
-    viewShare.click();
-  }
+
+savePlaylist.onclick = ()=>{
+    playAdd.click();
+}
+  
